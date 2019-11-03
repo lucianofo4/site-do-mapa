7e28c4efd5a2d71891d7c40a5320231772cf6933
luciano <lucianobf38@gmail.com>
2019-11-03 20:04:50 -0300

    a nova função e do xml no js. E para testar e procurar no mapbox o valor digitado pelo usuario

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-var cidade,resposta;
+var cidade,resposta,oReq,reqListener;
 
 function pegarcidade(){
     cidade = document.getElementById("digitado").value;
 }
+
+function testar (){
+    oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open("get","https://api.mapbox.com/geocoding/v5/mapbox.places/"+cidade+".json?access_token=sk.eyJ1Ijoib3J1YW4iLCJhIjoiY2sxYmEwbW53MDJpeDNvcGN4Mm5mYWYwciJ9.wuSyAqEfN8SFraG1v9jE8Q");
+    oReq.send();
+} 
+    
