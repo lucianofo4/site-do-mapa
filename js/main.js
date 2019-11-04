3c29d5d6aca53249809c52b21febdae766c6829a
luciano <lucianobf38@gmail.com>
2019-11-03 22:07:47 -0300

    o compiler

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var cidade,resposta,oReq,reqListener;
+var cidade,resposta,oReq,reqListener,obj,cordinate,
+cordinate1;
 
 function pegarcidade(){
     cidade = document.getElementById("digitado").value;
@@ -10,4 +11,12 @@ function testar (){
     oReq.open("get","https://api.mapbox.com/geocoding/v5/mapbox.places/"+cidade+".json?access_token=sk.eyJ1Ijoib3J1YW4iLCJhIjoiY2sxYmEwbW53MDJpeDNvcGN4Mm5mYWYwciJ9.wuSyAqEfN8SFraG1v9jE8Q");
     oReq.send();
 } 
-    
+function reqListener () {
+	resposta = this.responseText;  
+}
+function compiler (){
+	obj = (JSON.parse(resposta)	);
+    console.log (obj);
+    cordinate = obj.features[0].center[1];
+	cordinate1 = obj.features[0].center[0];
+}
